023a4cf56a61bb8baeab51fdec8f99742842d4c2
surendharsekar <47851494+surendharsekar@users.noreply.github.com>
2019-03-25 12:05:00 +0530

    Rename 9.c to 10.c

---
+++ player/set2/10.c
@@ -0,0 +1,31 @@
+
+#include<stdio.h>
+#include<string.h>
+int main() 
+{
+    char a[10];
+    int i,n;
+	scanf("%s",a);
+	n=strlen(a);
+	for(i=0;i<n;i++)
+	{
+	    if(a[i]=='X')
+	    {
+	  a[i]='A';
+	    }
+	 else if(a[i]=='Y')
+	 {
+	     a[i]='B';
+	    }
+ else if(a[i]=='Z')
+	  {
+	    a[i]='C';
+	    }
+	  else
+	    {
+	    a[i]=a[i]+3;  
+	    }
+	}
+	printf("%s",a);  
+	return 0;
+}
